a1194ff84c3637dcab86ce2b17dfb0d5170bc999
AlexFilip <AlexFilipWork@gmail.com>
2023-05-20 23:07:57 -0400

    Configure fugitive

--- lua/groups.lua
+++ lua/groups.lua
@@ -38,8 +38,8 @@ OpenInNewTab:autoCmd("FileType", { "help", "man" }, function()
     vim.cmd.wincmd("T")
 end)
 
-local WrapLinesGroup = M:makeGroup("WrapLines")
-WrapLinesGroup:autoCmd("FileType", { "txt", "markdown", "md", "notes", "org", "tex", "plaintex" }, function()
+local WrapLines = M:makeGroup("WrapLines")
+WrapLines:autoCmd("FileType", { "txt", "markdown", "md", "notes", "org", "tex", "plaintex" }, function()
     local bufnr = vim.api.nvim_get_current_buf()
 
     vim.api.nvim_set_option_value('wrap', true, { buf = bufnr })
@@ -47,37 +47,30 @@ WrapLinesGroup:autoCmd("FileType", { "txt", "markdown", "md", "notes", "org", "t
     vim.api.nvim_set_option_value('list', false, { buf = bufnr })
 end)
 
-local PythonGroup = M:makeGroup("Python")
-WrapLinesGroup:autoCmd("FileType", { "py" }, function()
+local Python = M:makeGroup("Python")
+Python:autoCmd("FileType", { "py" }, function()
     local bufnr = vim.api.nvim_get_current_buf()
     vim.api.nvim_set_option_value('tabstop', 4, { buf = bufnr })
 end)
 
 
-local TerraformGroup = M:makeGroup("Terraform")
-TerraformGroup:autoCmd("BufWritePost",  "*.tf", function() 
+local Terraform = M:makeGroup("Terraform")
+Terraform:autoCmd("BufWritePost",  "*.tf", function() 
     -- In lua anything that is not `false`, including 0, is true
     if vim.fn.executable("terraform") ~= 0 then
         io.popen("terraform fmt " .. vim.fn.expand("%"))
     end
 end)
 
-
-local EncryptedFileGroup  = M:makeGroup("EncryptedFile")
-EncryptedFileGroup:autoCmd({ "BufNewFile", "BufRead" }, "*.gpg", function() 
-    -- print("Hello")
-    -- TODO: Get file name and split it up by dots. If it is something like abc.org.gpg. Make it have the same filetype as if abc.org was opened
-    local filename = vim.fn.expand("%")
+local EncryptedFile  = M:makeGroup("EncryptedFile")
+EncryptedFile:autoCmd({ "BufNewFile", "BufRead" }, "*.gpg", function() 
+    -- local filename = vim.fn.expand("%")
+    -- filename = string.sub(filename, 1, #filename - #".gpg")
+    -- vim.o.filetype = vim.filetype.match { filename = filename }
 end)
-EncryptedFileGroup:autoCmd("User", "GnuPG", function() 
+EncryptedFile:autoCmd("User", "GnuPG", function() 
     vim.o.undofile = false
 end)
-
--- augroup encrypted_file
---   au!
---   autocmd BufNewFile,BufRead *.org.gpg set ft=org | set formatoptions-=cro
---   autocmd User GnuPG setlocal noundofile
--- augroup END
 
 local JSONComments = M:makeGroup("JSONComments")
 JSONComments:autoCmd("FileType", "json", function()
@@ -109,8 +102,8 @@ NeovimTerm:autoCmd("TermClose", "*", function()
     -- let g:term_statuses[b:terminal_job_id] = 0
 end)
 
-local FileHeadersGroup = M:makeGroup("FileHeaders")
-FileHeadersGroup:autoCmd("BufNewFile", { "*.c", "*.cpp", "*.h", "*.hpp" }, function() 
+local FileHeaders = M:makeGroup("FileHeaders")
+FileHeaders:autoCmd("BufNewFile", { "*.c", "*.cpp", "*.h", "*.hpp" }, function() 
     -- TODO:
     -- CreateSourceHeader()
 end)
