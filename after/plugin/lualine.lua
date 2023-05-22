79b24a3e15019c295a7700ee00ad7b4c1358e8a7
AlexFilip <AlexFilipWork@gmail.com>
2023-05-21 20:09:43 -0400

    Fix laststatus issue

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,67 +1,73 @@
 
 require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {{
-        'tabs',
-        max_length = vim.o.columns / 3, -- Maximum width of tabs component.
-        -- Note:
-        -- It can also be a function that returns
-        -- the value of `max_length` dynamically.
-        mode = 2,
-
-        -- Automatically updates active tab color to match color of other components (will be overidden if buffers_color is set)
-        use_mode_colors = false,
-
-        tabs_color = {
-            -- Same values as the general color option can be used here.
-            active = 'lualine_a_normal',     -- Color for active tab.
-            inactive = 'lualine_b_inactive', -- Color for inactive tab.
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+        disabled_filetypes = {
+            statusline = {},
+            winbar = {},
         },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        }
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {
+            {
+                'tabs',
+                max_length = vim.o.columns / 3, -- Maximum width of tabs component.
+                -- Note:
+                -- It can also be a function that returns
+                -- the value of `max_length` dynamically.
+                mode = 2,
 
-        -- fmt = function(name, context)
-        --     -- Show + if buffer is modified in tab
-        --     local buflist = vim.fn.tabpagebuflist(context.tabnr)
-        --     local winnr = vim.fn.tabpagewinnr(context.tabnr)
-        --     local bufnr = buflist[winnr]
-        --     local mod = vim.fn.getbufvar(bufnr, '&mod')
+                -- Automatically updates active tab color to match color of other components (will be overidden if buffers_color is set)
+                use_mode_colors = false,
 
-        --     return name .. (mod == 1 and ' +' or '')
-        -- end
-    }},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
+                tabs_color = {
+                    -- Same values as the general color option can be used here.
+                    active = 'lualine_a_normal',     -- Color for active tab.
+                    inactive = 'lualine_b_inactive', -- Color for inactive tab.
+                },
+
+                -- fmt = function(name, context)
+                    --     -- Show + if buffer is modified in tab
+                    --     local buflist = vim.fn.tabpagebuflist(context.tabnr)
+                    --     local winnr = vim.fn.tabpagewinnr(context.tabnr)
+                    --     local bufnr = buflist[winnr]
+                    --     local mod = vim.fn.getbufvar(bufnr, '&mod')
+
+                    --     return name .. (mod == 1 and ' +' or '')
+                    -- end
+                }
+            },
+            lualine_x = {'encoding', 'fileformat', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'}
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {}
+    }
+
+vim.o.showtabline = 0
+vim.o.laststatus  = 3
+vim.o.winbar      = "%f %m"
