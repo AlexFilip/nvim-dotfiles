87f82e56b9614601bbaf843add2b60d15e200631
AlexFilip <AlexFilipWork@gmail.com>
2023-06-02 20:34:21 -0400

    Move the RenameFiles function into lua (commented out for now) and delete init-old.vim

--- lua/commands.lua
+++ lua/commands.lua
@@ -6,25 +6,55 @@ local function createCommand(customCommand, opts, handler)
     end, opts)
 end
 
-createCommand("Q", { bang = true }, function(tbl)
-    vim.cmd{ cmd = "q", bang = tbl.bang }
-end)
-
-local function doQA(tbl)
-    vim.cmd { cmd = "qa" , bang = tbl.bang }
+function QWE_fn(str)
+    return function(tbl)
+        vim.cmd { cmd = str, bang = tbl.bang, args = tbl.args }
+    end
 end
-createCommand("Qa", { bang = true }, doQA)
-createCommand("QA", { bang = true }, doQA)
 
-createCommand("E", { bang = true, nargs = "?", complete = "file" }, function(tbl)
-    vim.cmd { cmd = "e", bang = tbl.bang, args = tbl.args }
-end)
+createCommand("Q", { bang = true }, QWE_fn("q"))
+createCommand("W", { bang = true, nargs = "?", complete = "file" }, QWE_fn("w"))
+createCommand("E", { bang = true, nargs = "?", complete = "file" }, QWE_fn("e"))
 
-createCommand("W", { bang = true, nargs = "?", complete = "file" }, function(tbl)
-    vim.cmd { cmd = "w", bang = tbl.bang, args = tbl.args }
-end)
+createCommand("Qa", { bang = true }, QWE_fn("qa"))
+createCommand("QA", { bang = true }, QWE_fn("qa"))
+createCommand("Wq", { bang = true, nargs = "?", complete = "file" }, QWE_fn("wq"))
+createCommand("WQ", { bang = true, nargs = "?", complete = "file" }, QWE_fn("wq"))
 
 createCommand("RmColor", {}, function(tbl)
     -- TODO: Save existing search, replace after performing current search then noh
     vim.cmd [[ %s/[[0-9;]*[mK]//g ]]
+end)
+
+
+createCommand("RenameFiles", {}, function()
+    -- local lines = filter(getline(1, '$'), {idx, val -> len(val) > 0})
+    -- local file_list = split(system("ls"), '\n')
+
+    -- if len(lines) != len(file_list) then
+    --     echoerr join(["Number of lines in buffer (", len(lines),
+    --                 \ ") does not match number of files in current directory (", \ len(file_list),
+    --                 \ ")"], "")
+    --     return
+    -- end
+
+    -- local commands = repeat([''], len(file_list))
+    -- for index in range(len(file_list)) do
+    --     -- TODO: replace characters that need escaping with \char
+    --     commands[index] = join(["mv \"", file_list[index], "\" \"", lines[index], "\""], "")
+    -- end
+
+    -- vim.cmd.normal "gg\"_dG"
+
+    -- Write value of `commands` to the buffer
+    --   `put` writes value of register to file
+    --   =<expr> treats an expression as a register
+    -- vim.cmd.put "=commands"
+
+    -- I would still have to make sure that all of the appropriate characters
+    -- in the filename, like quotes, are escaped.
+    --
+    -- Start by running :r !ls
+    -- Change names within the document
+    -- Run :w !zsh after this (use your shell of choice. Can get this with &shell)
 end)
