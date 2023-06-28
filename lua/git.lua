b9441cd111c20004ff3900c3f58fc26fda42f0d1
AlexFilip <AlexFilipWork@gmail.com>
2023-06-28 18:59:26 -0400

    Add git push and force push

--- lua/git.lua
+++ lua/git.lua
@@ -4,3 +4,6 @@ vim.keymap.set("n", "<leader>gd", function() vim.cmd 'vert Gdiffsplit!' end, { d
 vim.keymap.set("n", "<leader>gh", function() vim.cmd 'diffget //2' end, { desc = "Select left in conflict resolution" })
 vim.keymap.set("n", "<leader>gl", function() vim.cmd 'diffget //3' end, { desc = "Select right in conflict resolution" })
 
+vim.keymap.set("n", "<leader>gp", function() vim.cmd 'Git push' end, { desc = "git push" })
+vim.keymap.set("n", "<leader>gfp", function() vim.cmd 'Git push --force-with-lease' end, { desc = "git force push" })
+
