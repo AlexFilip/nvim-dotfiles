22070ef35be6de51cdb73559836ca4fb7148303c
AlexFilip <AlexFilipWork@gmail.com>
2023-09-14 13:31:57 -0400

    table merge and array replace util functions

--- lua/util.lua
+++ lua/util.lua
@@ -18,4 +18,31 @@ function exports.stringify(obj)
     return "{" .. keys .. "}"
 end
 
+function exports.tableMerge(t1, t2)
+    for k,v in pairs(t2) do
+        if type(v) == "table" then
+            if type(t1[k] or false) == "table" then
+                tableMerge(t1[k] or {}, t2[k] or {})
+            else
+                t1[k] = v
+            end
+        else
+            t1[k] = v
+        end
+    end
+    return t1
+end
+
+function exports.arrayReplace(array, value, newValue)
+    local newArray = {}
+    for _, v in pairs(array) do
+        if v == value then
+            newArray[#newArray+1] = newValue
+        else
+            newArray[#newArray+1] = v
+        end
+    end
+    return newArray
+end
+
 return exports
