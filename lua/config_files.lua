eb1c6664655408e47f641e075b9f6680eae31e6b
AlexFilip <AlexFilipWork@gmail.com>
2023-11-22 20:23:43 -0500

    Clean up

--- lua/config_files.lua
+++ lua/config_files.lua
@@ -1,12 +1,17 @@
 
+local json = require("jsonpath")
+
 local exports = {}
 
 function exports:getPath()
+    -- This plugin has trouble loading right away, DO NOT put this require at the top level
+    local yaml = require("yaml_nvim")
     local filetype = vim.bo.filetype
+
     if filetype == "json" then
-        return require("jsonpath").get()
+        return json.get()
     elseif filetype == "yaml" then
-        return require("yaml_nvim").get_yaml_key_and_value()
+        return yaml.get_yaml_key_and_value()
     end
 
     return ''
