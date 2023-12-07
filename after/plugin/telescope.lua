c281c1f7de80e1075844baa93d3645e96a0cb1f7
AlexFilip <AlexFilipWork@gmail.com>
2023-12-07 16:16:24 -0500

    Add projects plugin

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,10 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Search for files" })
 vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "Git search" })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Search buffers" })
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = "Search file contents" })
+
+telescope.load_extension('projects')
+
+vim.keymap.set('n', '<C-p>', function() telescope.extensions.projects.projects{} end, { desc = "Open recently opened project" })
