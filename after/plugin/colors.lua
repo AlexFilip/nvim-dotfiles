5b71db7d1bd2d9e2cebcbdd0e4344e8591c1f3fd
AlexFilip <work@alexfilip.com>
2024-12-29 16:14:28 -0500

    Remove unnecessary text

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -18,14 +18,6 @@ require('nord').set()
 local function SetTransparentBackground(color, theme)
     vim.o.background = theme or "dark"
     vim.cmd.colorscheme(color or "nord")
-
-    -- vim.api.nvim_set_hl(0, "Normal",       { ctermbg = "none", bg = "none" })
-    -- vim.api.nvim_set_hl(0, "NormalNC",     { ctermbg = "none", bg = "none" })
-    -- vim.api.nvim_set_hl(0, "NormalFloat",  { ctermbg = "none", bg = "none" })
-    -- vim.api.nvim_set_hl(0, "CursorLineNR", { ctermbg = "none", bg = "none" })
-    -- vim.api.nvim_set_hl(0, "CursorLine",   { ctermbg = "none", bg = "none" })
-    -- vim.api.nvim_set_hl(0, "NonText",      { ctermbg = "none", bg = "none", fg = "none" })
-    -- vim.api.nvim_set_hl(0, "EndOfBuffer",  { ctermbg = "none", bg = "none" })
 end
 
 SetTransparentBackground("nord", "dark")
