3605027a77cf7ce452067f3e1daaadcf31cd4577
AlexFilip <AlexFilipTech@pm.me>
2025-11-05 08:46:40 -0500

    Put all plugins in init.lua and remove some unnecessary stuff

--- init.lua
+++ init.lua
@@ -28,11 +28,41 @@ function ensure(repo, package, dir)
     vim.opt.runtimepath:prepend(install_path)
 end
 
--- ensure('Olical/aniseed', 'aniseed')
--- vim.g['aniseed#env'] = {module = 'init', compile = true}
--- require('aniseed.env').init()
-
 ensure('folke/lazy.nvim', 'lazy.nvim')
+
+--- Status line
+-- Count how many instances of a search term are found
+local function searchCount()
+    local search = vim.fn.searchcount({ maxcount = 0 }) -- maxcount = 0 makes the number not be capped at 99
+    local searchCurrent = search.current
+    local searchTotal = search.total
+    local result = ''
+    if searchCurrent > 0 then
+        -- result = '/' .. vim.fn.getreg('/') .. ' [' .. searchCurrent .. '/' .. searchTotal .. ']'
+        result = '[' .. searchCurrent .. '/' .. searchTotal .. ']'
+    end
+    return result
+end
+
+local winbar_config = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {
+        {
+            'filename',
+            file_status = true,     -- display monified, readonly, etc.
+            newfile_status = false, -- Display new file status (new file means no write after created)
+            path = 1,               -- Relative path
+            shorting_target = 40,   -- How many spaces to leave in window for other components
+        }
+    },
+    ---
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+}
+
+-- vim.o.laststatus = 0
 
 -- Leader and localleader mappings
 vim.g.mapleader = ' '
@@ -41,20 +71,18 @@ vim.g.maplocalleader = '\\'
 -- Setup lazy.nvim
 require('lazy').setup({
     spec = {
-        -- import your plugins
-        { import = 'plugins' },
+        -- { import = 'plugins' },
 
         -- Useful editor plugins
         {
             "kylechui/nvim-surround",
-            version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+            version = "^3.0.0",
             event = "VeryLazy",
             config = function()
-                require("nvim-surround").setup({
-                    -- Configuration here, or leave empty to use defaults
-                })
+                require("nvim-surround").setup({})
             end
         },
+
         {
             'mbbill/undotree',
             keys = {
@@ -69,19 +97,115 @@ require('lazy').setup({
                 { '<leader>gs', vim.cmd.Neogit, desc = 'Open git view' },
             },
             dependencies = {
-                "nvim-lua/plenary.nvim",         -- required
-                "sindrets/diffview.nvim",        -- optional - Diff integration
-
-                -- Only one of these is needed.
-                "nvim-telescope/telescope.nvim", -- optional
-                "ibhagwan/fzf-lua",              -- optional
-                "nvim-mini/mini.pick",           -- optional
-                "folke/snacks.nvim",             -- optional
+                "nvim-lua/plenary.nvim",
+                "sindrets/diffview.nvim",
+                "ibhagwan/fzf-lua",
             },
         },
+
+        {
+            'nvim-lualine/lualine.nvim',
+            dependencies = { 'nvim-tree/nvim-web-devicons' },
+            opts = {
+                options = {
+                    icons_enabled = true,
+                    theme = 'auto',
+                    component_separators = { left = '', right = ''},
+                    section_separators = { left = '', right = ''},
+                    disabled_filetypes = {
+                        statusline = {},
+                        winbar = {},
+                    },
+                    ignore_focus = {},
+                    always_divide_middle = true,
+                    globalstatus = true,
+                    -- refresh = { statusline = 1000, tabline = 1000, winbar  = 1000, }
+                },
+
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_c = {
+                        {
+                            'tabs',
+                            max_length = vim.o.columns, -- Maximum width of tabs component.
+                            -- Note:
+                            -- It can also be a function that returns
+                            -- the value of `max_length` dynamically.
+                            mode = 2,
+
+                            -- Automatically updates active tab color to match color of other components (will be overidden if buffers_color is set)
+                            use_mode_colors = false,
+
+                            tabs_color = {
+                                -- Same values as the general color option can be used here.
+                                active = 'lualine_a_normal',     -- Color for active tab.
+                                inactive = 'lualine_b_inactive', -- Color for inactive tab.
+                            },
+                        },
+                    },
+
+                    lualine_x = {{ searchCount }, 'encoding', { 'fileformat', icons_enabled = false }, 'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
+                },
+
+                always_show_tabline = false,
+                tabline = nil,
+
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = {'filename'},
+                    lualine_x = {'location'},
+                    lualine_y = {},
+                    lualine_z = {}
+                },
+
+                winbar = winbar_config,
+                inactive_winbar = winbar_config,
+                extensions = {}
+            }
+        },
+
+        {
+            "nvim-treesitter/nvim-treesitter",
+            branch = 'master',
+            lazy = false,
+            build = ":TSUpdate",
+            opts = {
+                -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+                ensure_installed = {
+                    "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline"
+                },
+
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = true,
+
+                -- List of parsers to ignore installing (or "all")
+                ignore_install = {},
+
+                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+                highlight = {
+                    enable = true,
+
+                    additional_vim_regex_highlighting = false,
+                },
+
+            },
+        },
+
         -- Theming
         {
             'shaunsingh/nord.nvim',
+            priority = 1000,
+            lazy = false,
             init = function(plugin)
                 vim.o.background = 'dark'
                 util.setValuesInObject(vim.g, {
@@ -107,57 +231,23 @@ require('lazy').setup({
                 vim.cmd.highlight('Visual',         'guibg=' .. named_colors.blue,  'gui=NONE')
                 vim.cmd.highlight('VisualNOS',      'guibg=' .. named_colors.blue,  'gui=NONE')
             end,
-            lazy = false,
-            priority = 1000,
-        },
-
-        -- Utilities
-        {
-            'nvim-telescope/telescope.nvim',
-            lazy = true,
-            branch = '0.1.x',
-            dependencies = {
-                { 'nvim-lua/plenary.nvim' },
-                { "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate" }
-            },
-            keys = {
-                { '<leader>ff', function(...) require('telescope.builtin').find_files(...) end, desc = 'Search for files' },
-                { '<leader>fg', function(...) require('telescope.builtin').git_files(...) end,  desc = 'Git search' },
-                { '<leader>fb', function(...) require('telescope.builtin').buffers(...) end,    desc = 'Search buffers' },
-                { '<leader>fs', function(...) require('telescope.builtin').live_grep(...) end,  desc = 'Search file contents' },
-                { '<leader>fm', function(...) require('telescope.builtin').marks(...) end,      desc = 'Search file contents' },
-                { '<leader>fr', function(...) require('telescope.builtin').registers(...) end,  desc = 'Search file contents' },
-            },
-        },
-
-        -- Lisp
-        {
-            'guns/vim-sexp',
-            lazy = true,
-            ft = { 'clojure', 'lisp', 'scheme' },
-        },
-
-        {
-            'Olical/conjure',
-            init = function(plugin)
-                vim.g['conjure#mapping#prefix'] = '<localleader>'
-            end,
-            lazy = true,
-            ft = { 'clojure', 'fennel' },
         },
     },
 
-    -- Configure any other settings here. See the documentation for more details.
-    -- colorscheme that will be used when installing plugins.
     install = {
         colorscheme = { 'nord.nvim' }
     },
 
-    -- automatically check for plugin updates
     checker = {
         enabled = true,
         notify = false
     },
+    change_detection = {
+        -- automatically check for config file changes and reload the ui
+        enabled = true,
+        notify = false, -- get a notification when changes are found
+    },
+
 })
 
 
